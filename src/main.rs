d4fe34b9a8f9b1f7b827920d7dc0b73d6f799149
Jacob Killelea <Jkillelea@protonmail.ch>
2018-04-24 14:59:15 -0600

    exit 1 on print_usage, misc formatting

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 extern crate dbus;
 
 use std::env;
+use std::process;
 use dbus::{Connection, BusType, Message};
 
 fn main() {
@@ -25,38 +26,39 @@ fn main() {
 
 fn print_usage() {
     println!("Usage: backlight_ctl {{up, down, get, set <val>}}");
+    process::exit(1);
 }
 
 fn get_percentage(c: &Connection) -> u32 {
     let timeout = 200; // milliseconds?
-    let msg = Message::new_method_call("org.gnome.SettingsDaemon.Power",
-                                        "/org/gnome/SettingsDaemon/Power",
-                                        "org.gnome.SettingsDaemon.Power.Screen",
-                                        "GetPercentage").unwrap();
+    let msg = Message::new_method_call("org.gnome.SettingsDaemon.Power",         // destination
+                                       "/org/gnome/SettingsDaemon/Power",       // path
+                                       "org.gnome.SettingsDaemon.Power.Screen", // interface
+                                       "GetPercentage").unwrap();               // method
     let resp = c.send_with_reply_and_block(msg, timeout);
     resp.unwrap().read1().unwrap()
 }
 
 fn set_percentage(c: &Connection, percent: u32) {
     let msg = Message::new_method_call("org.gnome.SettingsDaemon.Power",
-                                        "/org/gnome/SettingsDaemon/Power",
-                                        "org.gnome.SettingsDaemon.Power.Screen",
-                                        "SetPercentage").unwrap().append(percent);
+                                       "/org/gnome/SettingsDaemon/Power",
+                                       "org.gnome.SettingsDaemon.Power.Screen",
+                                       "SetPercentage").unwrap().append(percent);
     c.send(msg).unwrap();
 }
 
 fn step_down(c: &Connection) {
     let msg = Message::new_method_call("org.gnome.SettingsDaemon.Power",
-                                        "/org/gnome/SettingsDaemon/Power",
-                                        "org.gnome.SettingsDaemon.Power.Screen",
-                                        "StepDown").unwrap();
+                                       "/org/gnome/SettingsDaemon/Power",
+                                       "org.gnome.SettingsDaemon.Power.Screen",
+                                       "StepDown").unwrap();
     c.send(msg).unwrap();
 }
 
 fn step_up(c: &Connection) {
     let msg = Message::new_method_call("org.gnome.SettingsDaemon.Power",
-                                        "/org/gnome/SettingsDaemon/Power",
-                                        "org.gnome.SettingsDaemon.Power.Screen",
-                                        "StepUp").unwrap();
+                                       "/org/gnome/SettingsDaemon/Power",
+                                       "org.gnome.SettingsDaemon.Power.Screen",
+                                       "StepUp").unwrap();
     c.send(msg).unwrap();
 }
